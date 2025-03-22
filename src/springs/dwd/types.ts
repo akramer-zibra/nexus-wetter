c9dbacec13e92220672a4def6df2534b15ea89f5
Achim <achim@zibra.de>
2025-03-22 10:39:52 +0100

    rename index.d file to types

---
+++ src/springs/dwd/types.ts
@@ -0,0 +1,24 @@
+export interface Station {
+    name: string,
+    id: string,
+    code: string,
+    lat: number,
+    lng: number,
+    altitude: number,
+    end: string // e.g. 18.03.2025 // TODO rename to eol or activeness or last contact
+}
+
+export interface Forecast {
+    station: Station,
+    data: ForecastData
+}
+
+export interface ForecastData {
+    days: {
+        date: string,
+        temperatureMin: number,
+        temperatureMax: number,
+        precipitation: number,
+        icon: number,
+    }[]
+}
